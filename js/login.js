783d95d1f96d2c4c981c706d37c12d29a51b04ef
Nila Rani <neela9624@gmail.com>
2024-09-18 21:43:17 +0600

    added the partially the add money event handler

--- js/login.js
+++ js/login.js
@@ -7,4 +7,14 @@ document
 
     // step - 3: get the phone number
     const phoneNumber = document.getElementById('phone-number').value;
+    const pinNumber = document.getElementById('pin-number').value;
+
+    // step 4 : validate phone and pin
+    // this is temporary
+    if (phoneNumber === '017' && pinNumber === '1234') {
+      console.log('you are logged in');
+      window.location.href = '/home.html';
+    } else {
+      alert('wrong phone number or pin');
+    }
   });
